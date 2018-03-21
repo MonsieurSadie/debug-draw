d3bd72a99c589742faa819e934e0b962b0f6b60d
kevin <kevin@oneliferemains.com>
2018-03-22 00:17:32 +0100

    project for having unity gizmos like functions available in update scope

---
+++ Assets/Draw.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour {
+	static Mesh cubeMesh;
+	static Mesh quadMesh;
+	static Mesh cylinderMesh;
+	static Material debugMaterial;
+	static MaterialPropertyBlock materialPropertyBlock;
+	static int defaultLayer;
+	static float stroke = 0.03f;
+	static int circleDefinition = 40;
+
+	public static Color color{
+		set{ 
+			materialPropertyBlock.SetColor("_Color", value);
+		}
+	}
+
+	[RuntimeInitializeOnLoadMethod]
+	static void Initialize()
+	{
+		GameObject go = new GameObject("DrawDebug");
+		go.AddComponent<Draw>();
+
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cubeMesh = cube.GetComponent<MeshFilter>().mesh;
+		Destroy(cube);
+
+		GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		quadMesh = quad.GetComponent<MeshFilter>().mesh;
+		Destroy(quad);
+
+		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinderMesh = cylinder.GetComponent<MeshFilter>().mesh;
+		Destroy(cylinder);
+
+		debugMaterial = new Material(Shader.Find("Unlit/Color"));
+		materialPropertyBlock = new MaterialPropertyBlock();
+		defaultLayer = LayerMask.NameToLayer("Default");
+	}
+
+	void LateUpdate()
+	{
+		// clear color
+		color = Color.white;
+	}
+
+	public static void Line(Vector3 start, Vector3 end)
+	{
+		Vector3 line = end - start;
+		float length = line.magnitude;
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, line.normalized);
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(start + line*0.5f, rot, new Vector3(stroke, length * 0.5f, stroke));
+		Graphics.DrawMesh(cylinderMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
+	}
+
+	public static void WireRect(Vector3 pos, float w, float h)
+	{
+		Line(pos + Vector3.left * w * 0.5f + Vector3.up * h * 0.5f, pos + Vector3.right * w * 0.5f + Vector3.up * h * 0.5f);
+		Line(pos + Vector3.left * w * 0.5f - Vector3.up * h * 0.5f, pos + Vector3.right * w * 0.5f - Vector3.up * h * 0.5f);
+		Line(pos + Vector3.up * h * 0.5f + Vector3.left * w * 0.5f, pos + Vector3.down * h * 0.5f + Vector3.left * w * 0.5f);
+		Line(pos + Vector3.up * h * 0.5f + Vector3.right * w * 0.5f, pos + Vector3.down * h * 0.5f + Vector3.right * w * 0.5f);
+	}
+
+	public static void WireCircle(Vector3 pos, float radius)
+	{
+		float stepAngle = Mathf.PI*2 / circleDefinition;
+		Vector3 prevPos = pos;
+		prevPos.x += radius;
+		for (int i = 0; i <= circleDefinition; i++)
+		{
+			float angle = i * stepAngle;
+			Vector3 nextPos = pos;
+			nextPos.x += Mathf.Cos(angle) * radius;
+			nextPos.y += Mathf.Sin(angle) * radius;
+			Line(prevPos, nextPos);
+			prevPos = nextPos;
+		}
+	}
+
+	public static void Rect(Vector3 pos, float w, float h)
+	{
+		Quaternion rot = Quaternion.identity;
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, new Vector3(w, h, 1));
+		Graphics.DrawMesh(quadMesh, mat, debugMaterial, defaultLayer);
+	}
+
+
+	public static void Cube(Vector3 pos, Vector3 size)
+	{
+		Quaternion rot = Quaternion.identity;
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, size);
+		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer);
+	}
+
+	public static void Cube(Vector3 pos, Vector3 size, Quaternion rot)
+	{
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, size);
+		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer);
+	}
+
+}
---
+++ Assets/TestDraw.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDraw : MonoBehaviour {
+
+	public GameObject target;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Draw.DrawCube(Vector3.zero, Vector3.one * 2);
+		//Draw.DrawCube(Vector3.right, Vector3.one * 5, Quaternion.AngleAxis(45, Vector3.up));
+
+		//Draw.Rect(transform.position, 0.5f, 0.1f);
+
+		//Draw.color = Color.white;
+		Draw.Line(new Vector3(-2, 0,0), new Vector3(2, 0, 0));
+		Draw.color = Color.red;
+		Draw.Line(new Vector3(-2, -1,0), new Vector3(2, 2, 0));
+		Draw.color = Color.blue;
+		Draw.Line(new Vector3(0, -1,0), target.transform.position);
+		Draw.color = Color.gray;
+		Draw.WireRect(Vector3.one, 3, 1.5f);
+		Draw.color = new Color(0.9f, 0.5f, 0.2f);
+		Draw.WireCircle(Vector3.zero, 2);
+
+		//Draw.Line(new Vector3(-2, 0, 0), new Vector3(2, 2, 0));
+	}
+}
