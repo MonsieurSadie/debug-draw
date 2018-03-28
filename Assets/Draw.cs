754b95594553a8400f4364e9e5be9905019e698e
kevin <kevin@oneliferemains.com>
2018-03-28 22:13:29 +0200

    added a whole bunch of 2D and 3D shapes

--- Assets/Draw.cs
+++ Assets/Draw.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO : 
+// - find solution for alpha (can't use standard shader from script)
+// - find better solution for wireframe shaders
+// - wire cone2D and circle in 3D (giving an up vector or a quaternion as parameter)
+
 public class Draw : MonoBehaviour {
 	static Mesh cubeMesh;
 	static Mesh quadMesh;
+	static Mesh sphereMesh;
 	static Mesh cylinderMesh;
+	static Mesh circleMesh;
+
 	static Material debugMaterial;
+	static Material debugWireframeMaterial;
 	static MaterialPropertyBlock materialPropertyBlock;
+	static MaterialPropertyBlock wireframeMaterialPropertyBlock;
+	
 	static int defaultLayer;
 	static float stroke = 0.03f;
 	static int circleDefinition = 40;
@@ -15,6 +26,7 @@ public class Draw : MonoBehaviour {
 	public static Color color{
 		set{ 
 			materialPropertyBlock.SetColor("_Color", value);
+			wireframeMaterialPropertyBlock.SetColor("_WireColor", value);
 		}
 	}
 
@@ -32,12 +44,42 @@ public class Draw : MonoBehaviour {
 		quadMesh = quad.GetComponent<MeshFilter>().mesh;
 		Destroy(quad);
 
+		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphereMesh = sphere.GetComponent<MeshFilter>().mesh;
+		Destroy(sphere);
+
 		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
 		cylinderMesh = cylinder.GetComponent<MeshFilter>().mesh;
 		Destroy(cylinder);
 
+		/// Create a circle mesh
+		circleMesh = new Mesh();
+		Vector3[] vertices = new Vector3[circleDefinition+1];
+		int[] triangles = new int[circleDefinition*3];
+		float stepAngle = Mathf.PI*2 / circleDefinition;
+
+		vertices[0] = Vector3.zero;
+		vertices[1] = Vector3.right;
+		int triIndex = 0;
+
+		for (int i = 2; i <= circleDefinition; i++)
+		{
+			float angle = -i * stepAngle;
+			vertices[i] = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+
+			triangles[triIndex++]	= 0;
+			triangles[triIndex++] = i-1;
+			triangles[triIndex++]	= i;
+		}
+		circleMesh.vertices = vertices;
+		circleMesh.triangles = triangles;
+		circleMesh.RecalculateNormals();
+		/// end circle mesh
+
 		debugMaterial = new Material(Shader.Find("Unlit/Color"));
+		debugWireframeMaterial = new Material((Shader)Resources.Load("Wireframe"));
 		materialPropertyBlock = new MaterialPropertyBlock();
+		wireframeMaterialPropertyBlock = new MaterialPropertyBlock();
 		defaultLayer = LayerMask.NameToLayer("Default");
 	}
 
@@ -46,6 +88,13 @@ public class Draw : MonoBehaviour {
 		// clear color
 		color = Color.white;
 	}
+
+
+	//=================
+	//
+	// DRAW FUNCTIONS
+	//
+	//=================
 
 	public static void Line(Vector3 start, Vector3 end)
 	{
@@ -81,13 +130,42 @@ public class Draw : MonoBehaviour {
 		}
 	}
 
+	public static void WireCone2D(Vector3 pos, Vector3 direction, float radius, float angle)
+	{
+		float stepAngle = Mathf.PI*2 / circleDefinition;
+		Quaternion halfrot = Quaternion.Euler(0, 0, angle * 0.5f);
+		Vector3 prevPos = pos + halfrot * (direction * radius);
+		
+		Line(pos, prevPos);
+		for (float finalAngle = 0; finalAngle <= angle; finalAngle += stepAngle)
+		{
+			Vector3 nextPos = pos + Quaternion.Euler(0,0, angle * 0.5f - finalAngle) * direction * radius;
+			Line(prevPos, nextPos);
+			prevPos = nextPos;
+		}
+		Line(pos, prevPos);
+	}
+
+
+
 	public static void Rect(Vector3 pos, float w, float h)
 	{
 		Quaternion rot = Quaternion.identity;
 		Matrix4x4 mat = new Matrix4x4();
 		mat.SetTRS(pos, rot, new Vector3(w, h, 1));
-		Graphics.DrawMesh(quadMesh, mat, debugMaterial, defaultLayer);
+		Graphics.DrawMesh(quadMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
 	}
+
+	public static void Circle(Vector3 pos, float radius)
+	{
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, Quaternion.identity, Vector3.one);
+		Graphics.DrawMesh(circleMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
+	}
+
+
+
+
 
 
 	public static void Cube(Vector3 pos, Vector3 size)
@@ -95,14 +173,65 @@ public class Draw : MonoBehaviour {
 		Quaternion rot = Quaternion.identity;
 		Matrix4x4 mat = new Matrix4x4();
 		mat.SetTRS(pos, rot, size);
-		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer);
+		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
 	}
 
 	public static void Cube(Vector3 pos, Vector3 size, Quaternion rot)
 	{
 		Matrix4x4 mat = new Matrix4x4();
 		mat.SetTRS(pos, rot, size);
-		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer);
+		Graphics.DrawMesh(cubeMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
 	}
+
+	public static void Sphere(Vector3 pos, float radius)
+	{
+		Quaternion rot = Quaternion.identity;
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, Vector3.one * radius);
+		Graphics.DrawMesh(sphereMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
+	}
+
+	public static void Sphere(Vector3 pos, float radius, Quaternion rot)
+	{
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, Vector3.one * radius);
+		Graphics.DrawMesh(sphereMesh, mat, debugMaterial, defaultLayer, Camera.main, 0, materialPropertyBlock);
+	}
+
+
+
+
+	public static void WireCube(Vector3 pos, Vector3 size)
+	{
+		Quaternion rot = Quaternion.identity;
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, size);
+		Graphics.DrawMesh(cubeMesh, mat, debugWireframeMaterial, defaultLayer, Camera.main, 0, wireframeMaterialPropertyBlock);
+	}
+
+	public static void WireCube(Vector3 pos, Vector3 size, Quaternion rot)
+	{
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, size);
+		Graphics.DrawMesh(cubeMesh, mat, debugWireframeMaterial, defaultLayer, Camera.main, 0, wireframeMaterialPropertyBlock);
+	}
+
+
+	public static void WireSphere(Vector3 pos, float radius)
+	{
+		Quaternion rot = Quaternion.identity;
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, Vector3.one * radius);
+		Graphics.DrawMesh(sphereMesh, mat, debugWireframeMaterial, defaultLayer, Camera.main, 0, wireframeMaterialPropertyBlock);
+	}
+
+	public static void WireSphere(Vector3 pos, float radius, Quaternion rot)
+	{
+		Matrix4x4 mat = new Matrix4x4();
+		mat.SetTRS(pos, rot, Vector3.one * radius);
+		Graphics.DrawMesh(sphereMesh, mat, debugWireframeMaterial, defaultLayer, Camera.main, 0, wireframeMaterialPropertyBlock);
+	}
+
+
 
 }
--- Assets/TestDraw.cs
+++ Assets/TestDraw.cs
@@ -9,7 +9,7 @@ public class TestDraw : MonoBehaviour {
 		
 	}
 	
-	// Update is called once per frame
+	float angle = 0;
 	void Update () {
 		//Draw.DrawCube(Vector3.zero, Vector3.one * 2);
 		//Draw.DrawCube(Vector3.right, Vector3.one * 5, Quaternion.AngleAxis(45, Vector3.up));
@@ -18,15 +18,34 @@ public class TestDraw : MonoBehaviour {
 
 		//Draw.color = Color.white;
 		Draw.Line(new Vector3(-2, 0,0), new Vector3(2, 0, 0));
+
 		Draw.color = Color.red;
 		Draw.Line(new Vector3(-2, -1,0), new Vector3(2, 2, 0));
+
 		Draw.color = Color.blue;
 		Draw.Line(new Vector3(0, -1,0), target.transform.position);
+
 		Draw.color = Color.gray;
 		Draw.WireRect(Vector3.one, 3, 1.5f);
+
 		Draw.color = new Color(0.9f, 0.5f, 0.2f);
 		Draw.WireCircle(Vector3.zero, 2);
 
-		//Draw.Line(new Vector3(-2, 0, 0), new Vector3(2, 2, 0));
+		Draw.color = new Color(0.2f, 0.9f, 0.9f, 0.1f);
+		Draw.Rect(Vector3.zero, 1.5f, 2.2f);
+
+		Draw.color = Color.yellow;
+		Draw.Circle(Vector3.zero, 0.5f);
+
+		Draw.color = Color.green;
+		Draw.WireCube(-Vector3.one, Vector3.one, Quaternion.Euler(-30, 30, 0));
+
+		Draw.color = new Color(0.8f, 0.3f, 0.4f, 1f);
+		Draw.WireSphere(Vector3.right * 2, 1, Quaternion.Euler(0, 0, 30));
+		Draw.Sphere(Vector3.right * 3, 0.5f, Quaternion.Euler(0, 0, 30));
+
+		Draw.color = Color.white;
+		angle += 90 * Time.deltaTime;
+		Draw.WireCone2D(target.transform.position, Quaternion.Euler(0, 0, angle) * Vector3.right, 2, 45);
 	}
 }
